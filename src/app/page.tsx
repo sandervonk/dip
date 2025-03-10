6d0c983c175e10632a5b642c9bf546eec19c092a
Sander Vonk <10799950+sandervonk@users.noreply.github.com>
2025-03-09 22:59:06 -0400

    Standardize semantics

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,15 @@
 "use client";
+import React from "react";
 import { pages } from "../app/constants";
 
 export default function Home() {
   return (
-    <>
+    <React.Fragment>
       {pages.map((page, i) => {
         const Component = page.component;
         return Component && <Component key={i} data={page.data} />;
       })}
       <div style={{ height: 10000 }}>scroll me</div>
-    </>
+    </React.Fragment>
   );
 }
