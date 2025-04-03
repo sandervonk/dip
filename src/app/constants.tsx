2f925dfbbcbaf7b7969a4d797c6a1cbd4c3b4ff7
John H. Miller <miller@santafe.edu>
2025-04-03 16:22:36 -0600

    Update constants.tsx

--- src/app/constants.tsx
+++ src/app/constants.tsx
@@ -39,18 +39,18 @@ export const pages = [
           src: "/img/center/flowchart.png",
           alt: "Flowchart",
           blurbs: [
-            "*Frontier science* creates **a bottom-up solution**  to the Tragedy of the Commons.",
+            "*Frontier science* creates a **bottom-up solution** to the Tragedy of the Commons.",
             "*Integrating ideas* from agriculture, anthropology, chemistry, complex systems, computer science, earth science, ecology, engineering, economics, and finance.",
             "Driven by an *Information-Age infrastructure* that is **easy to scale with wide applicability.**",
-            "Alters incentives to **enhance the global commons** rather than destroy it.",
+            "Creates incentives to **enhance the global commons** rather than destroy it.",
           ],
         },
         {
           src: "/img/center/cylinder.png",
           alt: "Methane Measurement",
           blurbs: [
-            "*World class measurements* of the impacts and actions of environmental stewards.",
-            "Creating a new standard for documenting **world-enhancing activities.**",
+            "*World class measurements* of the impacts of environmental stewards.",
+            "Creating a *new standard* for tracking and documenting **world-improving activities.**",
           ],
         },
         {
@@ -64,7 +64,7 @@ export const pages = [
           src: "/img/center/remote-sensing-after.png",
           alt: "Sensing After",
           blurbs: [
-            "Providing high-quality results that **can be trusted** by patrons and markets.",
+            "Providing high-quality results that **can be trusted** by environmental patrons and markets.",
           ],
         },
       ],
@@ -75,7 +75,7 @@ export const pages = [
     data: {
       section: "World Improving Numbers",
       header: "WINS",
-      text: "Our remote sensing generates trustworthy WINs that translate the actions of the stewards into **measurable outcomes** such as tonnes of CO2e avoided or the amount of forest preserved in a natural state. High quality **WINS generate payments for the stewards** from various patrons and markets.",
+      text: "Remote sensing generates trustworthy WINs that translate the actions of the stewards into **measurable outcomes** such as tonnes of CO2e avoided or the amount of forest preserved in a natural state. High quality **WINS generate payments for the stewards** from global patrons and markets.",
     },
   },
   {
@@ -128,9 +128,9 @@ export const pages = [
           transform: 0.95,
         },
       ],
-      section: "A Pilot Project",
+      section: "Our pilot project",
       header: "Bali, Indonesia",
-      text: "Rice has been **sustainably farmed on Bali for over a millennium.**  Unfortunately, the flooded paddies produce methane, a dangerous greenhouse gas.  However, rice is water tolerant not water loving.  **Indigenous farmers can mitigate methane emissions** by changing how they irrigate their rice paddies, but such changes involve risk and extra effort. Incentive payments will help the farmers adapt their thousand-year old system of agriculture into a new regime of global sustainabilty.",
+      text: "Rice on Bali has been **sustainably farmed for over a millennium.**  Unfortunately, flooded paddies produce methane, a dangerous greenhouse gas.  By altering their irrigation practices, the **indigenous farmers can mitigate these methane emissions.**  Compensating the farmers for the additional risk and effort will allow them to transition to a new regime of global sustainabilty.",
     },
   },
   {
@@ -140,25 +140,26 @@ export const pages = [
       stats: [
         {
           stat: {
-            before: ">",
+            before: "More than ",
             number: 10,
             after: "%",
           },
-          text: "Rice farming *produces over 10%* of world-wide, anthropogenic methane emissions.",
+          text: "of *global anthropogenic methane emissions* come from rice farming.",
         },
         {
           stat: {
+            before: "Methan has ",
             number: 28,
             after: "x",
           },
-          text: "*Methane has 28 times the Global Warming Potential of CO2.* While methane has a limited lifetime in the atmosphere relative to CO2, the additional warming caused by *methane may push the global system beyond critical tipping points.*",
+          text: "the *Global Warming Potential of CO2.* While methane has a limited lifetime in the atmosphere, it could *push the global system beyond critical tipping points.*",
         },
         {
           stat: {
             number: 75,
             after: "%",
           },
-          text: "Methane emissions can be **reduced by over 75%** by not keeping the paddies continuously flooded.  This also reduces the runoff of fertilizer that damages Bali's coral reefs.",
+          text: "of **methane emissions can be eliminated** by not flooding the rice paddies.  This also reduces the runoff of fertilizer that damages Bali's coral reefs.",
         },
       ],
       media: {
