fc9bb26d854a0f52dffbb4f3a2fb87268f85a071
John H. Miller <miller@santafe.edu>
2025-04-07 06:10:40 -0600

    Update constants.tsx

--- src/app/constants.tsx
+++ src/app/constants.tsx
@@ -37,7 +37,7 @@ export const pages: PageData[] = [
   {
     component: WrappedTextHeader,
     data: {
-      section: "The Idea",
+      section: "*The idea*",
       header: "Realign individual incentives to reflect global impacts",
       text: "An automated system leverages remote sensing and international markets to enable **payments to indigenous stewards** who enhance the global commons.",
     },
