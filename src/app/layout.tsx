1d361284303005747b3e08fa92b365e3d2a63992
Sander Vonk <10799950+sandervonk@users.noreply.github.com>
2025-04-11 14:12:53 -0400

    Add back smooth scroll

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.scss";
 import { Cursor } from "motion-plus/react";
-// import SmoothScrolling from "@/components/SmoothScrolling";
+import SmoothScrolling from "@/components/SmoothScrolling";
 import Nav from "@/components/NavBar";
 import { GoogleAnalytics } from "@next/third-parties/google";
 export const dynamic = "error";
@@ -28,25 +28,25 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${inter.variable}`}>
-        {/* <SmoothScrolling> */}
-        <Cursor
-          style={{
-            backgroundColor: "var(--cursor)",
-            backgroundImage: "url(/img/cursor.webp)",
-            backgroundSize: "110%",
-            backgroundRepeat: "no-repeat",
-            backgroundPosition: "center",
-          }}
-          // add remove background on text varient with overwriting the rest of the style
-          variants={{
-            text: {
-              backgroundImage: "none",
-            },
-          }}
-        />
-        <Nav />
-        <main>{children}</main>
-        {/* </SmoothScrolling> */}
+        <SmoothScrolling>
+          <Cursor
+            style={{
+              backgroundColor: "var(--cursor)",
+              backgroundImage: "url(/img/cursor.webp)",
+              backgroundSize: "110%",
+              backgroundRepeat: "no-repeat",
+              backgroundPosition: "center",
+            }}
+            // add remove background on text varient with overwriting the rest of the style
+            variants={{
+              text: {
+                backgroundImage: "none",
+              },
+            }}
+          />
+          <Nav />
+          <main>{children}</main>
+        </SmoothScrolling>
       </body>
       <GoogleAnalytics gaId="G-R3W7N6X2MC" />
     </html>
